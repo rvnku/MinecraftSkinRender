5b91b83abf2536157c1cb44f923f61ebafaa64fc
rvnku <artpr.zxc@gmail.com>
2026-08-02 06:54:17 +0500

    change address

--- src/utils/skinApi.ts
+++ src/utils/skinApi.ts
@@ -2,7 +2,7 @@ import type { SkinSystem } from '../types';
 
 export async function getSkinUrl(system: SkinSystem, nickname: string): Promise<string | null> {
   const response = await fetch(
-    `http://176.108.254.84:5000/skins/api/${system.toLowerCase()}/${nickname}.png`
+    `https://rvnku.ru/skins/api/${system.toLowerCase()}/${nickname}.png`
   );
   if (!response.ok) return null;
   return response.url;
